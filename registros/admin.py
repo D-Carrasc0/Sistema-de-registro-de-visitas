0249df876e32dc5334eea82365392511e4739653
D-Carrasco <dixson.carrasco@inacapmail.cl>
2025-10-29 14:07:12 -0300

    feat: accion masiva

--- registros/admin.py
+++ registros/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
 from .models import Registro
+from django.utils import timezone
 
 # Register your models here.
-admin.site.register(Registro)
+@admin.action(description="Marcar salidas como completas")
+def marcar_salidas(modeladmin, request, queryset):
+    # Obtener la hora actual para la hora de salida
+    hora_actual = timezone.now()
 
-# @admin.register(Registro) 
-# class ProductoAdmin(admin.ModelAdmin): 
-#     list_display = ("nombre", "rut", "motivo", "horaentrada","horasalida", "estado") 
-#     search_fields = ("nombre", "rut")  # texto rápido 
-#     list_filter = ("estado",)           # filtros laterales 
-#     ordering = ("nombre",) 
-#     list_per_page = 25 
-#     autocomplete_fields = ()  # ej.: ("categoria",) si existiera FK grande 
+    # Actualizar las visitas seleccionadas
+    updated = queryset.update(estado=True, horasalida=hora_actual)
+
+    # Mostrar mensaje de confirmación
+    modeladmin.message_user(request, f"{updated} visitas han sido marcadas como completas.")
+
+@admin.register(Registro) 
+class ProductoAdmin(admin.ModelAdmin): 
+    list_display = ("nombre", "rut", "motivo", "horaentrada","horasalida") 
+    search_fields = ("nombre", "rut")  # texto rápido 
+    list_filter = ("horaentrada", "horasalida")           # filtros laterales 
+    ordering = ("nombre",) 
+    list_per_page = 25 
+    actions = [marcar_salidas]
